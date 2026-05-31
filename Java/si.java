edd39fc2bdd1d2da85e2ca52f6b551c324ec09fb
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-06-01 00:05:42 +0545

    java

--- Java/si.java
+++ Java/si.java
@@ -7,6 +7,9 @@ double t=Double.parseDouble(arg[1]);
 double r=Double.parseDouble(arg[2]);
 double si=(p*t*r)/100;
 System.out.println(" the simple interest of given data is="+si);
+// compund interest
+double ci=p*Math.pow((1+r/100),t)-p;
+System.out.println(" the compound interest of given data is="+ci);
 
 }
 }
