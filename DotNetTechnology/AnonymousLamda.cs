447dc4c11fc26a0cb8d2c6ca50e7ef2292c3ded5
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-06-02 21:38:36 +0545

    lab7

---
+++ DotNetTechnology/AnonymousLamda.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    public class AnonymousLamda
+    {
+        public class AnonymousEx
+        {
+            public delegate int adddelegate(int x, int y);
+            public static void Display()
+            {
+                Console.WriteLine("The Anynomous mrthod example");
+                adddelegate del = delegate (int x, int y) { return x + y; };
+                int res = del.Invoke(45, 75);
+                Console.WriteLine("The sum of 2 Number 45 and 75 using Anonymous Function = " + res);
+            }
+        }
+        public class LambdaEx
+        {
+          
+            public delegate int AreaRectangle(int l, int b);
+            public static void Display()
+            {
+                Console.WriteLine("The Lambda expression Example");
+                AreaRectangle a = (l, b) => l * b;
+                Console.WriteLine("Area of rectangel with l= 45 and b= 20 =" + a.Invoke(45, 20));
+                AreaRectangle p = (l, b) => { return 2 * (l + b); };
+                Console.WriteLine("Perimeter of Rectangle with l= 45 and b= 20 " + p.Invoke(45, 20));
+            }
+        }
+    }
+}
---
+++ DotNetTechnology/DelegateTypes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    public class DelegateTypes
+    {
+        public class SingleCast
+        {
+            public static int Square(int n)
+            {
+                return n * n;
+            }
+            public delegate int delsquare(int n);
+            public static void Display()
+            {
+                delsquare del = new delsquare(Square);
+                int res = del.Invoke(25);
+                Console.WriteLine("Square using Single Cast 25 =" + res);
+            }
+        }
+        public class MultiCast
+        {
+            public static void Square(int n)
+            {
+                int res = n * n;
+                Console.WriteLine("Square using MultiCast of " + n + " =" + res);
+            }
+            public static void Cube(int n)
+            {
+                int res = n * n * n;
+                Console.WriteLine("Cube using MultiCast of " + n + " =" + res);
+            }
+            public delegate void multidelegate(int x);
+            public static void Display()
+            {
+                multidelegate d1 = new multidelegate(Square);
+                multidelegate d2 = new multidelegate(Cube);
+                multidelegate d3 = d1 + d2;
+                d3.DynamicInvoke(15);
+                
+            }
+        }
+    }
+}
