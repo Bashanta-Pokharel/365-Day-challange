5f7a389a1739e8ffa628265f1b042a7fee46405b
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-06-02 22:47:15 +0545

    lab7

---
+++ DotNetTechnology/EventHandlingDeligate.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    public class EventHandlingDeligate
+    {
+        public class Person
+        {
+            public void SendMessage(string name, double op, double np)
+            {
+                Console.WriteLine($"The share price of {name} is changed from {op} to {np}");
+            }
+        }
+
+        public delegate void PriceChanged(string name, double op, double np);
+
+        public class StockTicker
+        {
+            public event PriceChanged pc;
+
+            public string name { get; set; }
+
+            private double price;
+
+            public double Price
+            {
+                get { return price; }
+                set
+                {
+                    double op = price;
+                    price = value;
+
+                    if (pc != null)
+                        pc.Invoke(name, op, price);
+                }
+            }
+        }
+
+        public static void Display()
+        {
+            Person p = new Person();
+            StockTicker st = new StockTicker();
+
+            st.name = "NABIL";
+            st.Price = 6000;
+
+            st.pc += p.SendMessage;
+
+            st.Price = 8000;
+            st.Price = 7500;
+        }
+    }
+}
---
+++ DotNetTechnology/PrebuiltGenericDelegate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    public class PrebuiltGenericDelegate
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static void Show(string name)
+        {
+            Console.WriteLine("Name = " + name);
+        }
+
+        public static bool CheckLength(string name)
+        {
+            return name.Length > 10;
+        }
+
+        public static void Display()
+        {
+            // Func Delegate
+            Func<int, int, int> d1 = new Func<int, int, int>(Add);
+            Console.WriteLine("Sum of two numbers using Func<> generic delegate = " + d1.Invoke(75, 99));
+
+            Console.WriteLine();
+
+            // Action Delegate
+            Action<string> d2 = new Action<string>(Show);
+            Console.WriteLine("This is from Action<> generic delegate ");
+             d2.Invoke("Bashanta Pokharel");
+
+            Console.WriteLine();
+
+            // Predicate Delegate
+            Predicate<string> d3 = new Predicate<string>(CheckLength);
+            Console.WriteLine("This is from Predicate<> generic delegate ");
+            bool result = d3.Invoke("Bashanta Pokharel");
+            Console.WriteLine("Length greater than 10 = " + result);
+        }
+    }
+}
