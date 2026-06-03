344b973e376bac4e816529e916ed7d2aea8e5138
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-06-03 21:52:28 +0545

    Update code

---
+++ DotNetTechnology/NameLengthExceptionaEx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace lab7
+{
+    public class NameLengthException : ApplicationException
+    {
+        public NameLengthException(string msg) : base(msg)
+        {
+        }
+    }
+
+    public class NameLengthExceptionEx
+    {
+        public static void Display()
+        {
+            try
+            {
+                Console.Write("Enter your name: ");
+                string name = Console.ReadLine();
+
+                if (name.Length > 10)
+                {
+                    throw new NameLengthException(
+                        "Name length cannot be greater than 10 characters."
+                    );
+                }
+
+                Console.WriteLine("Valid Name: " + name);
+            }
+            catch (NameLengthException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program Ended");
+            }
+        }
+    }
+}
---
+++ DotNetTechnology/SystemException.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace lab7
+{
+    public class SystemException
+    {
+        public class InvalidCastExceptionEx
+        {
+            public static void Show()
+            {
+                try
+                {
+                    Console.Write("Enter any text: ");
+                    object obj = Console.ReadLine();
+
+                    int num = (int)obj;
+
+                    Console.WriteLine(num);
+                }
+                catch (InvalidCastException ex)
+                {
+                    Console.WriteLine("Invalid Cast Exception: " + ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Code executed successfully.");
+                }
+            }
+        }
+
+        public class DivideByZeroExceptionEx
+        {
+            public static void Show()
+            {
+                try
+                {
+                    Console.Write("Enter first number: ");
+                    int a = int.Parse(Console.ReadLine());
+
+                    Console.Write("Enter second number: ");
+                    int b = int.Parse(Console.ReadLine());
+
+                    int c = a / b;
+
+                    Console.WriteLine("Result = " + c);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("Divide By Zero Exception: " + ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Code executed successfully.");
+                }
+            }
+        }
+
+        public class IndexOutOfRangeExceptionEx
+        {
+            public static void Show()
+            {
+                try
+                {
+                    int[] arr = { 10, 20, 30 };
+
+                    Console.Write("Enter array index: ");
+                    int index = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Value = " + arr[index]);
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine("Index Out Of Range Exception: " + ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Code executed successfully.");
+                }
+            }
+        }
+    }
+}
