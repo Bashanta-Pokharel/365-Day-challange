bb502e34a30ef1b25c43599dd75846fa95390fd5
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-06-07 14:15:06 +0545

    dot net database2

---
+++ DotNetTechnology/CustomerInsert.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace lab8i
+{
+    public class CustomerInsert
+    {
+        static void Main(string[] args)
+        {
+            string con =
+                "server=localhost;port=3336;database=bank;uid=root;password=;";
+
+            MySqlConnection conn = new MySqlConnection(con);
+
+            string insertquery =
+                "INSERT INTO customer(accountno, name, address, depositamount) VALUES " +
+                "(101, 'Basanta', 'Kathmandu', 1000)," +
+                "(102, 'Sweekriti', 'Lalitpur', 300)," +
+                "(103, 'Asish', 'Bhaktapur', 700)," +
+                "(104, 'Amit', 'Kirtipur', 400)," +
+                "(105, 'Kausal', 'Pokhara', 1200);";
+
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand(insertquery, conn);
+
+            int row = cmd.ExecuteNonQuery();
+
+            if (row > 0)
+                Console.WriteLine(row + " Rows Inserted Successfully");
+            else
+                Console.WriteLine("Error in the insertion");
+
+            conn.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DotNetTechnology/CustomerUpdate.cs
@@ -0,0 +1,37 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace lab8i
+{
+    public class CustomerUpdate
+    {
+        static void Main(string[] args)
+        {
+            string con =
+                "server=localhost;port=3336;database=bank;uid=root;password=;";
+
+            MySqlConnection conn = new MySqlConnection(con);
+
+            string updatequery =
+                "UPDATE customer SET " +
+                "address = 'Jorpati', " +
+                "depositamount = 2000 " +
+                "WHERE accountno = 101;";
+
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand(updatequery, conn);
+
+            int row = cmd.ExecuteNonQuery();
+
+            if (row > 0)
+                Console.WriteLine(row + " Row Updated Successfully");
+            else
+                Console.WriteLine("No Record Found to Update");
+
+            conn.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
