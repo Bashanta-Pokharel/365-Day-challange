38ff2a490e4e2fec3a2fe64ef4b30d790b9066a3
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-06-07 14:14:39 +0545

    dot net database

---
+++ DotNetTechnology/CustomerDelete.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace lab8i
+{
+    public class CustomerDelete
+    {
+        static void Main(string[] args)
+        {
+            string con =
+                "server=localhost;port=3336;database=bank;uid=root;password=;";
+
+            MySqlConnection conn = new MySqlConnection(con);
+
+            string deletequery =
+                "DELETE FROM customer WHERE depositamount < 500;";
+
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand(deletequery, conn);
+
+            int row = cmd.ExecuteNonQuery();
+
+            if (row > 0)
+                Console.WriteLine(row + " Rows Deleted Successfully (Deposit < 500)");
+            else
+                Console.WriteLine("No records found with deposit < 500");
+
+            conn.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DotNetTechnology/CustomerDisplay.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace lab8i
+{
+    public class CustomerDisplay
+    {
+        static void Main(string[] args)
+        {
+            string con =
+                "server=localhost;port=3336;database=bank;uid=root;password=;";
+
+            MySqlConnection conn = new MySqlConnection(con);
+
+            string query = "SELECT * FROM customer;";
+
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            Console.WriteLine("AccountNo\tName\tAddress\tDepositAmount");
+            Console.WriteLine("------------------------------------------------");
+
+            while (reader.Read())
+            {
+                Console.WriteLine(
+                    reader["accountno"] + "\t" +
+                    reader["name"] + "          " +
+                    reader["address"] + "          " +
+                    reader["depositamount"]);
+            }
+
+            conn.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
