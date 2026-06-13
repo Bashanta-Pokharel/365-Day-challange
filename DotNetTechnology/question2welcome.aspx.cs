50e9d9d9817df6dc4501f3d1711efdd2e2272c47
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-06-13 22:31:03 +0545

    dotnet web5

---
+++ DotNetTechnology/question2welcome.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace lab9
+{
+    public partial class question2welcome : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("question2.aspx");
+        }
+    }
+}
