58e32aa9c82c4f585f1bdce7a5a1a440912de9de
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-06-14 22:38:36 +0545

    dotnet web8

---
+++ DotNetTechnology/question3.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace lab9
+{
+    public partial class question3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                result.Text =
+                    "Registration Successful <br/><br/>" +
+                    "Name : " + txtname.Text + "<br/>" +
+                    "Email : " + txtemail.Text + "<br/>" +
+                    "Password : " + txtpassword.Text + "<br/>" +
+                    "Age : " + txtage.Text + "<br/>" +
+                    "Phone Number : " + txtphone.Text;
+            }
+        }
+    }
+}
