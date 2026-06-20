58b17a9a08072aafc6236d75552a26a3afacda66
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-06-20 22:09:38 +0545

    this is c practice 5

---
+++ c/4 table do while - Copy.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i=1;
+	do
+	{
+		printf("\n 4*%d=%d",i,4*i);
+		i++;
+ 	} 	while(i<=10);// exit control
+ 	do 
+ 	{
+ 		printf("\n4*%d=%d",i,4*i);
+ 		i++;
+ 		
+	 }while(i<=20);
+	return 0;
+}
