06d4da9feef79b1208922ae76a7cb2a1162efca9
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-06-27 02:05:46 +0545

    27junee22 22

---
+++ ScriptingLanguage/crud/crudheader.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <style>
+        body{
+            font-size: 20px;
+            background-color: white;
+            border: 4px solid red;
+            color: black;
+            text-align: center;
+            size: 40px;
+            justify-items: center;
+            padding: 20px;
+            margin: 20px;
+            font-family: Arial, sans-serif;
+        }
+       
+   </style>
+</head>
+<body>
+    <h1 > THis is the student details</h1>
+    
+</body>
+<footer><p>created by<br>Bashanta pokharel</p> </footer>
+</html>
