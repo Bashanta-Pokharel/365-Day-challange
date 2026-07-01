4e15f2161fca9c2d852821caa51f323c152f7108
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-07-01 21:14:23 +0545

    july 1 iv

---
+++ Java/maxmin.java
@@ -0,0 +1,28 @@
+
+public class maxmin
+{
+public static void main(String[]arg)
+{
+int []n={10,5,3,15,12,35,25,6};
+int min = n[0];
+int max= n[0];
+System.out.println("array before finding max min");
+for(int i=1;i<n.length;i++)
+{
+System.out.print(" "+n[i]);
+}
+for(int i=1;i<n.length;i++)
+{
+if (min>n[i])
+{
+min=n[i];
+}
+if (max<n[i])
+{
+max=n[i];
+}
+}
+System.out.println("");
+System.out.println("minimum = " +min+ " maximum = " +max);
+}
+}
