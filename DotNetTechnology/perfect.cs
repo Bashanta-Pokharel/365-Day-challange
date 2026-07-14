fa1349b8878d5242e4da8f7cd04ddc9fed47115e
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-07-14 23:54:52 +0545

    acaff

--- DotNetTechnology/perfect.cs
+++ DotNetTechnology/perfect.cs
@@ -10,21 +10,4 @@ namespace lab2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the number to check perfect number or not:");
-            int n = int.Parse(Console.ReadLine());
-            int sum = 0;
-            for (int i = 1; i < n; i++)
-            {
-                if (n % i == 0)
-                    sum = sum + i;
-
-            }
-            if (sum == n)
-                Console.WriteLine(n+" is the perfect number.");
-            else
-                Console.WriteLine(n+" is not the Perfect number.");
-            Console.ReadKey();
-
-        }
-    }
-}
+         
