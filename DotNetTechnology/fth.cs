f814bdc7c1cdaaecd893c07ce1bf34d2506b55ff
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-08-02 22:10:44 +0545

    the task is completed

--- DotNetTechnology/fth.cs
+++ DotNetTechnology/fth.cs
@@ -1,9 +1,7 @@
 using System;
 public class temperature
 {
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Enter the degree celcius:");
+    pnsole.WriteLine("Enter the degree celcius:");
         float c = float.Parse(Console.ReadLine());
 	float f = ((180*c)/100)+32;
 	Console.WriteLine("the degree foreinheight is :"+f);
